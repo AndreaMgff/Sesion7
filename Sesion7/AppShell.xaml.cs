034a2510ffefdba0245e8d634c41ea84d801539a
UBU <UBU@CEE-AI-C11-S06>
2025-03-27 17:31:09 +0100

    Agregar archivos de proyecto.

---
+++ Sesion7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Sesion7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sesion7/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Sesion7
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent(); //siempre tiene que estar el primero
+            Time();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private async void Time()
+        {
+            while (true)
+            {
+                RelojLBL.Text = DateTime.Now.ToString("HH:mm:ss");
+                await Task.Delay(1000); //esto es en milisegundos, espera un segundo
+            }
+        }
+    }
+
+}
