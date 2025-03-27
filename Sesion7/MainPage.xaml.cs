8d7461e5a0d2da7673b4c78f7b75251943f3f56d
UBU <UBU@CEE-AI-C11-S06>
2025-03-27 18:46:59 +0100

    Progreso barra

--- Sesion7/MainPage.xaml.cs
+++ Sesion7/MainPage.xaml.cs
@@ -1,13 +1,33 @@
-﻿namespace Sesion7
+﻿using System.Threading;
+
+namespace Sesion7
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
+        ProgressBar progressBar;
+        Thread[] tasks;
+        private readonly object locker = new object(); //solo lectura
         public MainPage()
         {
             InitializeComponent(); //siempre tiene que estar el primero
-            Time();
+            progressBar = new ProgressBar //Para añadir elemento en tiempo de ejecucion
+
+            {
+                Progress = 0,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.Center
+            };
+            Layout.Children.Add(progressBar); //coger los hijos del layout y los añadimos
+
+            Progreso();
+            tasks = new Thread[100];
+            for (int i = 0; i <= 1000; i++) 
+            {
+                tasks[i] = new Thread(() => Texto(i.ToString()));
+                tasks[i].Start();
+            }
+
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -28,6 +48,23 @@
                 RelojLBL.Text = DateTime.Now.ToString("HH:mm:ss");
                 await Task.Delay(1000); //esto es en milisegundos, espera un segundo
             }
+        }
+        private async void Progreso()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                progressBar.Progress = i/100.0;
+                await Task.Delay(500);
+            }
+        }
+        private async void Texto(String txt)
+        {
+            await Task.Delay(5000);
+            lock (locker)
+            {
+                RelojLBL.Text = txt;
+            }
+            
         }
     }
 
